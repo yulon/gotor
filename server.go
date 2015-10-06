16c9445eb677914500278dd337b68009c6d6bb57
Yuchen Wang <yulon@me.com>
2015-10-07 04:31:13 +0800

    Add server.go

---
+++ server.go
@@ -0,0 +1,23 @@
+package hs
+
+import (
+	"net/http"
+)
+
+type Handler func(http.ResponseWriter, *http.Request)
+
+type slh struct{
+	h Handler
+}
+
+func (s *slh) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.h(w, r)
+}
+
+func StandLibHandler(h Handler) (http.Handler) {
+	return &slh{h}
+}
+
+func New(addr string, h Handler) {
+	http.ListenAndServe(addr, StandLibHandler(h))
+}
