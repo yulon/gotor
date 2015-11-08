b92281ed45f5b54ba79d3e8df1028a222f9f90f9
Yuchen Wang <yulon@me.com>
2015-11-08 13:19:56 +0100

    Remove StdLibHandler functions

--- server.go
+++ server.go
@@ -8,28 +8,22 @@ type Handler func(http.ResponseWriter, *http.Request)
 
 type slh struct{
 	h Handler
-	s string
 }
 
 func (s *slh) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	r.URL.Scheme = s.s
 	s.h(w, r)
 }
 
-func StdLibHandler(h Handler) http.Handler {
-	return &slh{h, ""}
-}
-
-func StdLibHandlerHaveScheme(h Handler, scheme string) http.Handler {
-	return &slh{h, scheme}
+func ToStdHandler(h Handler) http.Handler {
+	return &slh{h}
 }
 
 func New(addr string, h Handler) {
-	http.ListenAndServe(addr, StdLibHandlerHaveScheme(h, "http"))
+	http.ListenAndServe(addr, ToStdHandler(h))
 }
 
 func NewTLS(addr string, certFile string, keyFile string, h Handler) {
-	http.ListenAndServeTLS(addr, certFile, keyFile, StdLibHandlerHaveScheme(h, "https"))
+	http.ListenAndServeTLS(addr, certFile, keyFile, ToStdHandler(h))
 }
 
 var NotFound Handler = http.NotFound
