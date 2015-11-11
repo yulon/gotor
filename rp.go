426b8f26213079e95bfbb5d2909aebb441544eca
Yuchen Wang <yulon@me.com>
2015-11-11 01:02:51 +0100

    Added ProxyRedirect()

--- rp.go
+++ rp.go
@@ -6,18 +6,45 @@ import (
 	"net/url"
 )
 
-func ProxyPass(w http.ResponseWriter, r *http.Request, target string) {
-	t, _ := url.Parse(target)
-	rp := httputil.NewSingleHostReverseProxy(t)
-	r.Host = t.Host
-	rp.ServeHTTP(w, r)
-}
-
-func ProxyPassHandler(target string) Handler {
-	t, _ := url.Parse(target)
+func ProxyPass(target string) Handler {
+	t, err := url.Parse(target)
+	if err != nil {
+		return nil
+	}
 	rp := httputil.NewSingleHostReverseProxy(t)
 	return func(w http.ResponseWriter, r *http.Request) {
 		r.Host = t.Host
 		rp.ServeHTTP(w, r)
+	}
+}
+
+type proxyRedirectResponseWriter struct{
+	http.ResponseWriter
+	tarURL *url.URL
+	reqURL *url.URL
+}
+
+func (prrw *proxyRedirectResponseWriter) WriteHeader(status int) {
+	rustr := prrw.Header().Get("Location")
+	if rustr != "" {
+		ru, err := url.Parse(rustr)
+		if err != nil && ru.Scheme == prrw.tarURL.Scheme && ru.Host == prrw.tarURL.Host && len(ru.RawPath) >= len(prrw.tarURL.Path) && ru.RawPath[:len(prrw.tarURL.Path)] == prrw.tarURL.Path {
+			ru.Host = prrw.reqURL.Host
+			ru.RawPath = ru.RawPath[len(prrw.tarURL.Path):]
+			prrw.Header().Set("Location", ru.String())
+		}
+	}
+	prrw.ResponseWriter.WriteHeader(status)
+}
+
+func ProxyRedirect(target string) Handler {
+	t, err := url.Parse(target)
+	if err != nil {
+		return nil
+	}
+	rp := httputil.NewSingleHostReverseProxy(t)
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.Host = t.Host
+		rp.ServeHTTP(&proxyRedirectResponseWriter{w, t, r.URL}, r)
 	}
 }
