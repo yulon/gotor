0a088294460fe4081b189e1030c48bcb6343df42
Yuchen Wang <yulon@me.com>
2015-12-24 03:54:13 +0100

    Rename

--- fs.go
+++ fs.go
@@ -113,7 +113,7 @@ func (fs *FileService) Single(w http.ResponseWriter, r *http.Request, fileName s
 	}
 }
 
-func (fs *FileService) BindDir(rootDir string) http.HandlerFunc {
+func (fs *FileService) Bridge(rootDir string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		fs.Single(w, r, filepath.Join(rootDir, r.URL.Query().Get("*")))
 	}
