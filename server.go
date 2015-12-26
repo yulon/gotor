25e3f935e5de685db3d40205f33d715c6eb645a7
Yuchen Wang <yulon@me.com>
2015-12-27 00:45:32 +0100

    Added CGI & FastCGI server

--- server.go
+++ server.go
@@ -1,15 +1,30 @@
 package gotor
 
 import (
+	"net"
 	"net/http"
+	"net/http/cgi"
+	"net/http/fcgi"
 )
 
-func Run(addr string, hf http.HandlerFunc) {
-	http.ListenAndServe(addr, hf)
-}
-
-func RunTLS(addr string, certFile string, keyFile string, hf http.HandlerFunc) {
-	http.ListenAndServeTLS(addr, certFile, keyFile, hf)
-}
-
 var NotFound http.HandlerFunc = http.NotFound
+
+func HTTP(addr string, hf http.HandlerFunc) error {
+	return http.ListenAndServe(addr, hf)
+}
+
+func HTTPS(addr string, certFile string, keyFile string, hf http.HandlerFunc) error {
+	return http.ListenAndServeTLS(addr, certFile, keyFile, hf)
+}
+
+func CGI(hf http.HandlerFunc) error {
+	return cgi.Serve(hf)
+}
+
+func FastCGI(addr string, hf http.HandlerFunc) error {
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	return fcgi.Serve(ln, hf)
+}
